7e770247f6b438064a6e17a11e289a496cb0a53a
Bha-Gu <bhaumik050402@gmail.com>
2023-06-21 11:52:15 +0530

    modified:   src/binary.rs 	modified:   src/linear.rs 	modified:   src/main.rs 	modified:   src/root.rs

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,13 @@ mod root;
 use root::root;
 
 fn main() {
-    let arr = vec![1, 2, 4,23, 5, 6, 3, 7, 9, 8,93];
+    let arr = vec![1, 2, 4, 23, 5, 6, 3, 7, 9, 8, 93];
     let mut sorted = arr.clone();
     sorted.sort();
-    println!("L: {:?}\nB: {:?}\nR: {:?}", linear(arr, 3), binary(sorted.clone(), 3), root(sorted, 3));
+    println!(
+        "L: {:?}\nB: {:?}\nR: {:?}",
+        linear(arr, 3),
+        binary(sorted.clone(), 3),
+        root(sorted, 3)
+    );
 }
--- src/root.rs
+++ src/root.rs
@@ -1,11 +1,12 @@
 ///Square root search
 /// Runtime: O(√N)
-pub fn root<T>(arr: Vec<T>, val: T) -> Option<usize> 
-where T: PartialOrd,
-{   
+pub fn root<T>(arr: Vec<T>, val: T) -> Option<usize>
+where
+    T: PartialOrd,
+{
     let length = arr.len();
     let jump = (length as f64).sqrt().floor() as usize;
-    //let mut i =jump; 
+    //let mut i =jump;
     // i = loop {
     //     if i >= length || arr[i] >= val {
     //         break i;
@@ -13,7 +14,7 @@ where T: PartialOrd,
     //     i += jump;
     // };
     // i -= jump
-    let i = (jump..length).step_by(jump).find(|&j| arr[j] >= val)? - jump; 
+    let i = (jump..length).step_by(jump).find(|&j| arr[j] >= val)? - jump;
     // for j in i..length.min(i + jump + 1) {
     //     if arr[j] == val {
     //         return Some(j);
