fe4666e4e0580cec2a58f6b78d0f853a1eaa45fc
Bha-Gu <bhaumik050402@gmail.com>
2023-08-24 13:07:33 +0530

    commenting binary.rs

--- src/binary.rs
+++ src/binary.rs
@@ -4,20 +4,35 @@ pub fn binary<T>(arr: &Vec<T>, val: &T) -> Option<usize>
 where
     T: PartialOrd + Clone,
 {
+    //lower pointer
     let mut lo: usize = 0;
+    //higher pointer
     let mut hi: usize = arr.len();
+    //middle pointer
     let mut m: usize = (lo + hi) / 2;
+    //current item
     let mut curr: T = arr[m].to_owned();
+    //looping till found or end
     while lo < hi {
+        //found it
         if *val == curr {
             return Some(m);
-        } else if curr > *val {
+        }
+        //it could be in bottom segment
+        else if curr > *val {
+            //update top pointer
             hi = m;
-        } else {
+        }
+        //it could be in top segment
+        else {
+            //update bottom pointer
             lo = m + 1;
         }
+        //update middle pointer
         m = (lo + hi) / 2;
+        //update current item
         curr = arr[m].to_owned();
     }
+    //not found
     None
 }
